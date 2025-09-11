ead5af899930f71f5a4c7a61029072dade9f4e87
Alejandro <alejandropontev@gmail.com>
2025-09-11 16:32:12 -0500

    subiendo datos al array

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let tusamigos=[];
+
+function agregarAmigo(){
+    dato=document.getElementById("amigo");
+    tusamigos.push('dato');
+    console.log(tusamigos);
+    
+}
