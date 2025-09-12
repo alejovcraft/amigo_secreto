defeb5bfe1b0268f4dee897e256571cff6cc4eab
Alejandro <alejandropontev@gmail.com>
2025-09-12 11:16:46 -0500

    en proceso

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ let tusamigos=[];
 
 function agregarAmigo(){
     dato=document.getElementById("amigo").value;
-    tusamigos.push(dato);
-    console.log(tusamigos);
-    document.querySelector
+    if(dato==""){
+        alert("debe poner un amigo porfavor :D")
+    }
+    else{
+        tusamigos.push(dato);
+        console.log(tusamigos);
+        clear();
+
+    }
+}
+
+function clear(){
+    dato=document.getElementById("amigo").value="";
+}
+function recorrerarray(){
+    contador=0;
+    while(contador>tusamigos.length-1){
+        document.getElementById("listaAmigos").value=<li>tusamigos[contador]</li>
+        contador++;
+    }
 }
