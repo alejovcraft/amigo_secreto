c32d3f48bf1d5620ddff8c2f0aa0e1fc6bdb8ed5
Alejandro <alejandropontev@gmail.com>
2025-09-16 14:15:49 -0500

    encontrando el error parte 1

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let tusamigos=[];
 
+let amigo=document.getElementById("amigo").value;
+let listaamigos=document.getElementById("listaAmigos").value;
+let botonagregar=document.querySelector(".button-add");
 function agregarAmigo(){
-    dato=document.getElementById("amigo").value;
-    if(dato==""){
+    if(amigo==""){
         alert("debe poner un amigo porfavor :D")
     }
     else{
         tusamigos.push(dato);
         console.log(tusamigos);
         clear();
-
     }
-}
+}S
 
 function clear(){
     dato=document.getElementById("amigo").value="";
 }
-function recorrerarray(){
-    contador=0;
+function monstrarlista(){
+    contador=0
     while(contador>tusamigos.length-1){
         document.getElementById("listaAmigos").value=<li>tusamigos[contador]</li>
         contador++;
     }
 }
+
+botonagregar.addEventListener("click",agregarAmigo);
